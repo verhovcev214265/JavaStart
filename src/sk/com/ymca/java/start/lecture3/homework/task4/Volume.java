2b46e1067d935a37dd24cdaa555f6f520133a548
verhovcev214265 <verhovcev214265@gmail.com>
2024-04-11 15:09:25 +0200

    additional task for Vlada & Maks

--- src/sk/com/ymca/java/start/lecture3/homework/task4/Volume.java
+++ src/sk/com/ymca/java/start/lecture3/homework/task4/Volume.java
@@ -1,9 +1,17 @@
 package sk.com.ymca.java.start.lecture3.homework.task4;
 
+/*
+Створіть клас Volume.
+Напишіть програму розрахунку об'єму - V та площі поверхні -S циліндра.
+Об'єм V циліндра радіусом – R та висотою – h, обчислюється за формулою: V = πR2h
+Площа поверхні циліндра обчислюється за формулою:
+S = 2πR2 + 2πR2 = 2πR(R+h)
+Результати розрахунків виведіть на екран.
+ */
+
 public class Volume {
 
     public static void main(String[] args) {
-
 
         double r = 10.0;
         double h = 4.0;
